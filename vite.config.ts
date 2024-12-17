9815f7d9d9007de9a4497d0b9ecb9e027db4d520
Rojames Binoya <r.binoya@jobtarget.com>
2024-12-18 01:11:59 +0800

    Added: base in vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/roj/', // This sets the base path for the app
 })
